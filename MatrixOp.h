f3da1745cf49caaf6b90ed704342354a03517ca6
LIU <cc960122@icloud.com>
2025-12-19 21:28:09 +0800

    PRINT

--- MatrixOp.h
+++ MatrixOp.h
@@ -12,6 +12,6 @@ int det(int A[SIZE][SIZE]);
 void adjoint(int A[SIZE][SIZE],int C[SIZE][SIZE]); 
 int inverse(int A[SIZE][SIZE],double C[SIZE][SIZE]); 
 void print(int A[SIZE][SIZE]); 
-void print_double(double A[SIZE][SIZE]); 
+void double(double A[SIZE][SIZE]); 
 #endif     
  
